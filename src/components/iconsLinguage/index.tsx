863a1e071c1b907bb976a13079fbb1ce375731d3
MrMa-rio <112214759+MrMa-rio@users.noreply.github.com>
2023-06-04 13:10:52 -0300

    Update index.tsx

--- src/components/iconsLinguage/index.tsx
+++ src/components/iconsLinguage/index.tsx
@@ -13,7 +13,7 @@ import { tailwindB, tailwindO,tailwindWM,
 
 export const IconsLinguage = (props:{toggleScreen:Function, togglePopup:Function, darkMode:any}) =>{
 
-  const [screen, setScreen] = useState<any>()
+  const [screen, setScreen] = useState<any>(window.innerWidth)
   useEffect(() => {
     const handleResize = () => {
       setScreen(window.innerWidth);
