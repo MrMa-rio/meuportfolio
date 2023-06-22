7db55b58ff93dd8f84b493d341992025f7f572a0
MrMa-rio <112214759+MrMa-rio@users.noreply.github.com>
2023-06-21 22:26:44 -0300

    fix: path Typescript

--- src/components/projects/Main/imports.tsx
+++ src/components/projects/Main/imports.tsx
@@ -3,7 +3,7 @@ import  {Tailwind}  from "./TailwindCSS"
 import { Javascript } from "./Javascript"
 import { Html } from "./Html"
 import { Css } from "./Css"
-import { typescript } from "./typescript"
+import { Typescript } from "./Typescript"
 import { ReactJs } from "./ReactJs"
 
-export {CSharp, Tailwind, Javascript, Html, Css, typescript, ReactJs}
+export {CSharp, Tailwind, Javascript, Html, Css, Typescript, ReactJs}
